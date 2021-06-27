e41c770ee0d3f1fd97ae7b8922bebf6edaa228d5
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-06-27 23:50:24 +0530

    Updates for 2021-06-27 23:50:24

---
+++ apple.py
@@ -0,0 +1,94 @@
+
+# assignment statement
+# a = 'banana is a great food \
+#     dsfdahj\
+#     dsadsad\
+#     12345321'
+
+# print statement
+
+# a = 5
+
+# b = 10
+   
+# print(a,b)
+
+# a,b,c = 5,10,15
+
+# print(a,b,c)
+
+# a = 5
+
+# b = 20
+
+# print(a,b)
+
+# a,b = b,a
+
+# print(a,b)
+
+# 1b2 = 10  # do not do this
+
+# print = 10
+
+# print(print) # never do this
+
+
+
+# type function 
+
+# a = 3.14
+
+# print(type(a))
+
+# print(my_list)
+
+# print(type(my_list))
+
+# apple = banana[1] + banana[2] + banana[0] + banana[4] + banana[3]
+
+# banana = [10,20,30,40,50]
+
+# print(banana) # list b4 changing
+
+# banana[1] = banana[0] + banana[2]
+
+# banana[2] = 69
+
+# print(banana) # list after changing
+
+# orange =  (1,2,3,10,40)
+
+# print(orange)
+
+# print(type(orange))
+
+# print(orange[3])
+
+# orange[1] = 100 # u cannot do this
+
+# grapes = list(orange)
+
+# print(type(orange))
+
+# print(type(grapes))
+
+# watermelon = "baloons"
+
+# print(type(watermelon))
+
+# watermelon[2] = "z" # u cannot do this
+
+# print(watermelon)
+
+# apples = [10,20,30,40,50,60,90,100]
+
+# print(apples[:])
+
+
+
+
+
+
+
+
---
+++ base.py
@@ -0,0 +1,5 @@
+x = 34 - 23
+y = 'hello'
+z = 3.14
+
+print(x,y,z)
