adeddf5544a1ee802981d0d49f7c407439a80a57
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-07-01 02:08:41 +0530

    Updates for 2021-07-01 02:08:41

---
+++ banana.py
@@ -0,0 +1,51 @@
+dict_1 = {'a':1, 'b':2, 'c':3}
+
+dict_2 = {'a':10, 'b':20, 'c':30}
+
+# print(dict_1['c'])
+
+new_dict = {
+    'name'  :"josh",
+    'age'   : 21,
+    'gender':'Male'
+    }
+
+# print(new_dict)
+
+# new_dict['name'] = "jack"
+
+# print(new_dict)
+
+# new_dict.clear()
+
+# print(new_dict)
+
+# new_dict = {
+#     'name'  :"josh",
+#     'age'   : 21,
+#     'gender':'Male'
+#     }
+
+# print(new_dict.items())
+
+# x = True
+# y = False
+
+# print(not y)
+
+# x = 110 
+
+# if x < 100:
+#     print('yes')
+# else:
+#     print('1231231e243')
+
+x = 10
+
+while x<20:
+    print(x)
+    x = x+3
+    break
+    print('a')
+
+print('done')
