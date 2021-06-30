799a979d47ffbf0ab60c89c7799b24dbc47732a8
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-07-01 01:10:06 +0530

    Updates for 2021-07-01 01:10:06

---
+++ orange.py
@@ -0,0 +1,61 @@
+# list_1 = [10,20,30,'choco','jack',100]
+# list_2 = [20,60,'dark']
+
+# print('length of list_1 ',len(list_1))
+
+# print('b4 appending: ', list_1, )
+
+# list_1.append(list_2)
+
+# print('after appending: ', list_1)
+
+# print('length of list_1 ',len(list_1))
+
+# list_1.insert(3,list_2)
+
+# print('after inserting: ', list_1)
+
+# list_1.extend(list_2)
+
+# print('after extending: ', list_1)
+
+# print('length of list_1 ',len(list_1))
+
+# list_1 = [10,20,30,'choco','jack',100,20,20]
+# list_2 = [20,60,'dark']
+
+# print(list_1.index('choco'))
+
+# print(list_1.count(20))
+
+# print(list_1)
+
+# list_1.remove(30)
+
+# print(list_1)
+
+# list_1 = [10,20,30,200,70,100,20,20]
+# list_2 = [20,60,'dark']
+
+# list_1.reverse()
+
+# print(list_1)
+
+# list_1.sort()
+# list_1.reverse()
+
+# print(list_1)
+
+list_1 = [10,20,30,200,70,100,20,20]
+list_2 = [20,60,'dark']
+
+# DICTIONARIES STARTED
+
+# a=1
+# b=2
+# c=3
+# print(a,b,c)
+
+dict_1 = {'a':1, 'b':2, 'c':3}
+
+print(dict_1)
