5ecdcee22821b3860afd5285053fea88555a96ba
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-07-03 15:35:51 +0530

    Updates for 2021-07-03 15:35:51

---
+++ pineapple.py
@@ -0,0 +1,53 @@
+# my_list = [12,45,100,190]
+
+# for element in my_list:
+#     if element < 80:
+#         print(element+10)
+#     else:
+#         print(element-10)
+
+my_string = "python is best,python is the thing"
+
+# count = 1
+
+# for char in my_string:
+#     if char == 't':
+#         count /= 2  #count = count + 1
+
+# print(count)
+
+
+# a = a+b
+
+# a+=b
+
+good_list = [(1,2),(3,4),(5,6),(7,8),(9,10)]
+
+# print(len(good_list))
+
+# for i,j in good_list:
+#     print(i+j)
+
+# food = range(10)
+
+# # print(range(10))
+
+# for i in range(10,21,5):
+#     print(i)
+
+# my_list = [12,45,100,190]
+
+# for (i, item) in enumerate(my_list):
+
+#     print (i, item)
+
+# new_list = [i*i for i in my_list if i == 100 ]
+
+# print(new_list)
+
+# my_list = [12,45,100,190]
+
+# new_list = [elem*2 for elem in [item+1 for item in my_list]]
+
+# print(new_list)
+
